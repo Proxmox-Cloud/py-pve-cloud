48bf47074df25a0a9bdb7a55018f6f4f8805e067
tobi <tobi_h@outlook.com>
2025-12-21 13:25:49 +0000

    dev ci

--- src/pve_cloud/lib/validate.py
+++ src/pve_cloud/lib/validate.py
@@ -1,9 +1,14 @@
 import paramiko
 import yaml
 import pve_cloud._version
+import os
 
 
 def raise_on_py_cloud_missmatch(proxmox_host):
+    # dont raise in tdd
+    if os.getenv("PYTEST_CURRENT_TEST"):
+        return
+
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(proxmox_host, username="root")
@@ -12,9 +17,6 @@ def raise_on_py_cloud_missmatch(proxmox_host):
     _, stdout, _ = ssh.exec_command("cat /etc/pve/cloud/cluster_vars.yaml")
 
     cluster_vars = yaml.safe_load(stdout.read().decode('utf-8'))
-
-    if pve_cloud._version.__version__.startswith("0.0."): # tddog development version, skip raising
-        return
   
     if cluster_vars["py_pve_cloud_version"] != pve_cloud._version.__version__:
         raise RuntimeError(f"Version missmatch! py_pve_cloud_version for cluster is {cluster_vars['py_pve_cloud_version']}, while you are using {pve_cloud._version.__version__}")
