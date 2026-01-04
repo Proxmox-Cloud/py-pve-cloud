ab23ed02aa0a2a919d271b48556d5c19c6bd50aa
tobi <tobi@tobias-huebner.tech>
2026-01-04 13:03:05 +0000

    Format and cleanup pipeline

--- src/pve_cloud/lib/validate.py
+++ src/pve_cloud/lib/validate.py
@@ -1,7 +1,8 @@
-import paramiko
-import yaml
-import pve_cloud._version
 import os
+
+import paramiko
+import pve_cloud._version
+import yaml
 
 
 def raise_on_py_cloud_missmatch(proxmox_host):
@@ -16,7 +17,9 @@ def raise_on_py_cloud_missmatch(proxmox_host):
     # since we need root we cant use sftp and root via ssh is disabled
     _, stdout, _ = ssh.exec_command("cat /etc/pve/cloud/cluster_vars.yaml")
 
-    cluster_vars = yaml.safe_load(stdout.read().decode('utf-8'))
-  
+    cluster_vars = yaml.safe_load(stdout.read().decode("utf-8"))
+
     if cluster_vars["py_pve_cloud_version"] != pve_cloud._version.__version__:
-        raise RuntimeError(f"Version missmatch! py_pve_cloud_version for cluster is {cluster_vars['py_pve_cloud_version']}, while you are using {pve_cloud._version.__version__}")
+        raise RuntimeError(
+            f"Version missmatch! py_pve_cloud_version for cluster is {cluster_vars['py_pve_cloud_version']}, while you are using {pve_cloud._version.__version__}"
+        )
